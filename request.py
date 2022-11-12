76b85476f663d79931aebdb83d7f41c9e927a4f0
nralt <1305289754@qq.com>
2022-11-12 21:54:02 +0800

    update

--- request.py
+++ request.py
@@ -2,4 +2,8 @@ import requests
 
 response = requests.get("http://gitlab.tcjk.com/api/v4/projects")
 
-print(response.headers.get('Date'))
+
+
+print(response.content)
+print(response)
+print(response.headers)
