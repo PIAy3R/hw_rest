4c5bafd655654ae0dd7e74014a167459971a45fe
nralt <1305289754@qq.com>
2022-11-30 17:05:23 +0800

    update

--- nlp.py
+++ nlp.py
@@ -3,8 +3,19 @@ from parseLogs import *
 import spacy
 import re
 
+def classify(TestCase:list):
+    for cases in TestCase:
+        if 'does not have a valid value' in re.findall(r'does not have a valid value', cases.response_body):
+            print('param value invalid')
+        elif 'is invalid' in re.findall(r'is invalid', cases.response_body):
+            print('param invalid')
+        elif '404' in re.findall(r'404', cases.response_body):
+            continue
+        if 'only' in re.findall(r'only', cases.response_body):
+            cases.showSelf()
+            print()
+
+
 if __name__ == '__main__':
     parseTestCases()
-    for cases in TestCases:
-        if re.match('is invalid', cases.response_body) != None:
-            print(cases.response_body)
+    classify(TestCases)
