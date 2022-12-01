9e5861c8f0aa12d88df2a360c9afdde5250dc78e
nralt <1305289754@qq.com>
2022-12-01 16:15:43 +0800

    update

--- Dto/graphdatatype.py
+++ Dto/graphdatatype.py
@@ -2,17 +2,29 @@ from hw_rest.Exception import exceptions
 from hw_rest.parseLogs import *
 from hw_rest.Dto.testcase import *
 
-class TestNodes:
-    def __init__(self,testcase:Testcase):
-        self.operation = testcase.operation
-        self.tags = testcase.tags
-        self.url = testcase.url
-        self.response_code = testcase.response_code
-        self.response_body = testcase.response_body
-        self.judge = testcase.judge
 
-    #
-    # @classmethod
-    # def build(cls, Operation, testInteractions, testResults):
-    #     testcase = Testcase.buildCase(Operation, testInteractions, testResults)
-    #     return cls(testcase)
+# class Nodes:
+#     def __init__(self):
+#         pass
+
+
+class TestNodes():
+    def __init__(self,operation, tags, url, response_code, response_body, judge):
+        self.operation = operation
+        self.tags = tags
+        self.url = url
+        self.response_code = response_code
+        self.response_body = response_body
+        self.judge = judge
+
+
+    @classmethod
+    def build(cls, testcase:Testcase):
+        operation = testcase.operation
+        tags = testcase.tags
+        url = testcase.url
+        response_code = testcase.response_code
+        response_body = testcase.response_body
+        judge = testcase.judge
+
+        return cls(operation, tags, url, response_code, response_body, judge)
--- graph.py
+++ graph.py
@@ -6,30 +6,36 @@ import os
 from parseLogs import *
 from Dto.graphdatatype import *
 
-operationList = []
 
 graph = Graph('bolt://localhost:7687/neo4j', auth=("pzy", "pzy123456"))
 matcher = NodeMatcher(graph) #创建关系需要用到
 
-
-parseTestCases()
-
 #create operation
-for cases in TestCases:
-    if cases.operation not in operationList:
-        operationList.append(cases.operation)
-for operations in operationList:
-    graph.create(Node('Operation', operation = operations))
+def createOpera():
+    operationList = []
+    for cases in TestCases:
+        if cases.operation not in operationList:
+            operationList.append(cases.operation)
+    for operations in operationList:
+        graph.create(Node('Operation', operation = operations))
 
 
-for cases in TestCases:
-    testnode = TestNodes(cases)
-    if testnode.response_code != 404:
-        operaNode = matcher.match('Operation', operation = testnode.operation).first()
-        node = Node('TestCase', operation=testnode.operation, url=testnode.url, status_code=testnode.response_code, responsebody=testnode.response_body,judge=testnode.judge)
-        graph.create(node)
-        relation = Relationship(node, 'belongs to', operaNode)
-        graph.create(relation)
+
+def createTestcase():
+    for cases in TestCases:
+        testnode = TestNodes.build(cases)
+        if testnode.response_code >= 200 and testnode.response_code < 400 and 'mutated' not in testnode.tags:
+            operaNode = matcher.match('Operation', operation = testnode.operation).first()
+            node = Node('TestCase', operation=testnode.operation, url=testnode.url, status_code=testnode.response_code, responsebody=testnode.response_body,judge=testnode.judge)
+            graph.create(node)
+            relation = Relationship(node, 'success case', operaNode)
+            graph.create(relation)
 
 def test():
     pass
+
+
+if __name__ == '__main__':
+    parseTestCases()
+    createOpera()
+    createTestcase()
